77c86831b7e758a7471af94cc1b81b17972ee4c4
Leonardo Restrepo <willeores@gmail.com>
2023-02-07 14:04:19 -0500

    test 2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(AppModule.port);
   await app.listen(AppModule.port);
 }
 bootstrap();
